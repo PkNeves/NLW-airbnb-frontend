24d223fe0c4f4c315ad6287d078406d137c4fd88
Petterson Neves <pettersonkesler@gmail.com>
2022-09-09 01:26:54 -0300

    feat: criacao do restante do html da pagina inicial

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ function App() {
   return (
     <div className="container">
       <img src={logo} alt="Airbnb"></img>
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> pra programadores e encontre <strong>talentos</strong> para sua empresa
+        </p>
+
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input type="email" id="email" placehodler="Seu melhor e-mail"/>
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
