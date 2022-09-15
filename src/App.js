9628608e4e4caa7146f07fa2b0cade31c58ce4c4
Petterson Neves <pettersonkesler@gmail.com>
2022-09-14 23:33:23 -0300

    feat: usando variavel de estado do react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import React, { useState } from 'react'
 import './App.css';
 import api from './services/api'
 
-import logo from './assets/logo.svg'
 // como a logo é uma variável, ela é usada entre chaves
+import logo from './assets/logo.svg'
 
 function App() {
+  // o useState serve para criar variaveis de estado dentro do React
+  // por padrao temos 2 variaveis, a primeira é a variável em sí e a segunda é uma função para alterar o valor da variavel
+  const [ email, setEmail ] = useState('')
   function handleSubmit(event) {
     event.preventDefault() 
-    console.log('Hello World')
+    console.log(email)
   }
 
   return (
@@ -21,7 +25,14 @@ function App() {
 
         <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
-          <input type="email" id="email" placehodler="Seu melhor e-mail"/>
+          <input 
+            id="email" 
+            type="email" 
+            placehodler="Seu melhor e-mail"
+            value={email}
+            // o js tem por padrão na variavel event da funcao onchange o valor que foi alterado atraves do event.target.value
+            onChange={event => setEmail(event.target.value)} 
+          />
           <button className="btn" type="submit">Entrar</button>
         </form>
       </div>
