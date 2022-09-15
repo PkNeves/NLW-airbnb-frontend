69514c3b7356aacf61d458f5eaa76638f254b523
Petterson Neves <pettersonkesler@gmail.com>
2022-09-14 23:45:16 -0300

    feat: salvar id do usuario no localStorage

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ function App() {
     const response = await api.post('/sessions', { email })
 
     const { _id } = response.data
-    console.log(_id)
+
+    localStorage.setItem('user', _id)
   }
 
   return (
