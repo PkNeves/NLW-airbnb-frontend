563029ccaf296556311d80539fa20e6fccdcc264
Petterson Neves <pettersonkesler@gmail.com>
2022-10-02 23:44:06 -0300

    refactor: levar arquivos especificos de login para arquivo de login

--- src/App.js
+++ src/App.js
@@ -1,46 +1,19 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css';
-import api from './services/api'
 
 // como a logo é uma variável, ela é usada entre chaves
 import logo from './assets/logo.svg'
 
+import Routes from './routes'
+
 function App() {
-  // o useState serve para criar variaveis de estado dentro do React
-  // por padrao temos 2 variaveis, a primeira é a variável em sí e a segunda é uma função para alterar o valor da variavel
-  const [ email, setEmail ] = useState('')
-  
-  async function handleSubmit(event) {
-    event.preventDefault() 
-
-    const response = await api.post('/sessions', { email })
-
-    const { _id } = response.data
-
-    localStorage.setItem('user', _id)
-  }
-
   return (
     <div className="container">
       <img src={logo} alt="Airbnb"></img>
 
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> pra programadores e encontre <strong>talentos</strong> para sua empresa
-        </p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL *</label>
-          <input 
-            id="email" 
-            type="email" 
-            placehodler="Seu melhor e-mail"
-            value={email}
-            // o js tem por padrão na variavel event da funcao onchange o valor que foi alterado atraves do event.target.value
-            onChange={event => setEmail(event.target.value)} 
-          />
-          <button className="btn" type="submit">Entrar</button>
-        </form>
+        <Routes />
+        
       </div>
     </div>
   );
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import api from '../../services/api'
+
 
 export default function Login() {
-  return <div />
+  // o useState serve para criar variaveis de estado dentro do React
+  // por padrao temos 2 variaveis, a primeira é a variável em sí e a segunda é uma função para alterar o valor da variavel
+  const [ email, setEmail ] = useState('')
+  
+  async function handleSubmit(event) {
+    event.preventDefault() 
+
+    const response = await api.post('/sessions', { email })
+
+    const { _id } = response.data
+
+    localStorage.setItem('user', _id)
+  }
+
+  return (
+    <>
+      <p>
+        Ofereça <strong>spots</strong> pra programadores e encontre <strong>talentos</strong> para sua empresa
+      </p>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">E-MAIL *</label>
+        <input 
+          id="email" 
+          type="email" 
+          placehodler="Seu melhor e-mail"
+          value={email}
+          // o js tem por padrão na variavel event da funcao onchange o valor que foi alterado atraves do event.target.value
+          onChange={event => setEmail(event.target.value)} 
+        />
+        <button className="btn" type="submit">Entrar</button>
+      </form>
+    </>
+  )
 }
