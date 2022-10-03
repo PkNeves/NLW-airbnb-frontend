435b819edb5c439a6826812db98b4a5b2d550825
Petterson Neves <pettersonkesler@gmail.com>
2022-10-03 01:29:11 -0300

    feat: usando os dados vindos do banco de dados na pagina de dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,17 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import api from '../../services/api'
 
 export default function Dashboard() {
+  const [spots, setSpots] = useState([])
   useEffect(() => {
     async function loadSpots() {
       const user_id = localStorage.getItem('user')
       const response = await api.get('/dashboard', {
         headers: { user_id }
       })
-      console.log(response.data)
+      setSpots(response.data)
     }
 
     loadSpots()
   }, [])
-  return <div />
+  return (
+    <>
+      <ul className='spot-list'>
+        {spots.map(spot => (
+          <li key={spot._id}>
+            <header style={{ backgroundImage: `url(${spot.thumbnail_url})`}}></header>
+            <strong>{spot.company}</strong>
+            <span>{spot.price}</span>
+          </li>
+        ))}
+
+      </ul>
+    </>
+  )
 }
