4c42fe51755402eb5137efbd89d5cd7ab0d94835
Petterson Neves <pettersonkesler@gmail.com>
2022-10-12 20:46:45 -0300

    feat: uso do Link do react para navegar entre paginas internas

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import api from '../../services/api'
 
 import './styles.css'
@@ -26,8 +27,10 @@ export default function Dashboard() {
             <span>{spot.price ? `R$${spot.price}/dia` : `GRATUITO`}</span>
           </li>
         ))}
-
       </ul>
+      <Link to="/new">
+        <button className='btn'>Cadastrar novo spot</button>
+      </Link>
     </>
   )
 }
